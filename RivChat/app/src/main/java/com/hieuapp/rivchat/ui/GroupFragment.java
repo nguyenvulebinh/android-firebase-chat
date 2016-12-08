4d91619709b52a4af939ae59cd9951ba20ffa836
Nguyen Binh <nguyenvulebinh@gmail.com>
2016-12-08 12:23:38 +0700

    Xong giao diện danh sách chat nhóm

--- RivChat/app/src/main/java/com/hieuapp/rivchat/ui/GroupFragment.java
+++ RivChat/app/src/main/java/com/hieuapp/rivchat/ui/GroupFragment.java
@@ -1,18 +1,30 @@
 package com.hieuapp.rivchat.ui;
 
+import android.content.Context;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import com.hieuapp.rivchat.MainActivity;
 import com.hieuapp.rivchat.R;
+import com.hieuapp.rivchat.model.ListFriend;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 /**
  * Created by hieuttc on 05/12/2016.
  */
 
 public class GroupFragment extends Fragment {
+    RecyclerView recyclerListGroups;
 
     public GroupFragment() {
         // Required empty public constructor
@@ -26,8 +38,41 @@ public class GroupFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_group, container, false);
+        View layout = inflater.inflate(R.layout.fragment_group, container, false);
+        recyclerListGroups = (RecyclerView) layout.findViewById(R.id.recycleListGroup);
+        GridLayoutManager layoutManager = new GridLayoutManager(getContext(), 2);
+        recyclerListGroups.setLayoutManager(layoutManager);
+        ListGroupsAdapter adapter = new ListGroupsAdapter(getContext());
+        recyclerListGroups.setAdapter(adapter);
+        return layout;
     }
 
+}
+class ListGroupsAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    private Context context;
+    public ListGroupsAdapter(Context context){
+        this.context = context;
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.rc_item_group, parent, false);
+        return new ItemFriendViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+    }
+
+    @Override
+    public int getItemCount() {
+        return 10;
+    }
+}
+
+class ItemGroupViewHolder extends RecyclerView.ViewHolder {
+    ItemGroupViewHolder(View itemView) {
+        super(itemView);
+    }
 }
