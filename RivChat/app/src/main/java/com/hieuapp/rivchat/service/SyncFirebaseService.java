bc77b1711a14e7dfa2270f368d73609fb2023a31
hieuapp <trunghieu.bka57@gmail.com>
2016-12-17 07:06:42 +0700

    them docs activity diagram

--- RivChat/app/src/main/java/com/hieuapp/rivchat/service/SyncFirebaseService.java
+++
@@ -1,9 +0,0 @@
-package com.hieuapp.rivchat.service;
-
-/**
- * Created by hieuttc on 05/12/2016.
- */
-
-
-public class SyncFirebaseService {
-}
--- RivChat/app/src/main/java/com/hieuapp/rivchat/ui/dialog/DialogUtils.java
+++
@@ -1,8 +0,0 @@
-package com.hieuapp.rivchat.ui.dialog;
-
-/**
- * Created by hieuttc on 05/12/2016.
- */
-
-public class DialogUtils {
-}
